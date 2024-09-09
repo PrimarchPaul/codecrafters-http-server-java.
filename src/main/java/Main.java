9da8beeb2d54a02e352f197a7092a1a38c4c8864
PrimarchPaul <reyespaul56@gmail.com>
2024-09-08 22:00:03 -0400

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,19 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import httpServer.*;
 
 public class Main {
+  static ServerSocket serverSocket;
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
 
 
     try {
       serverSocket = new ServerSocket(4221);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
+      while (true) {
+        new HTTPServer(serverSocket.accept());
+      }
+
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
