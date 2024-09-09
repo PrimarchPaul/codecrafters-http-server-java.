860a57f981a2270f854177d2a9baddb96e177f20
PrimarchPaul <reyespaul56@gmail.com>
2024-09-08 22:01:13 -0400

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,39 +18,6 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
 
-      // Read the request from the client
-      InputStream input = clientSocket.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-      String line = reader.readLine();
-      System.out.println(line);
-      String[] httpRequest = line.split(" ");
-
-
-
-      OutputStream output = clientSocket.getOutputStream();
-      if (httpRequest[1].equals("/")) {
-        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      } else if (httpRequest[1].startsWith("/echo/")) {
-        String reqMsg = httpRequest[1].substring(6);
-        String header = String.format(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-                reqMsg.length(), reqMsg);
-        clientSocket.getOutputStream().write(header.getBytes());
-      }else if (httpRequest[1].startsWith("/user-agent")) {
-        String userAgent = null;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-          if (line.startsWith("User-Agent:")) {
-            userAgent = line.substring("User-Agent:".length()).trim();
-          }
-        }
-        String header = String.format(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-                userAgent.length(), userAgent);
-        clientSocket.getOutputStream().write(header.getBytes());
-      }else {
-        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-      }
-
     } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
     }
